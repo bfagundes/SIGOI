c53bcc394fd2b6d411d40e3ec0dccbfac0a66533
Bruno Fagundes <brunofs8@gmail.com>
2015-09-02 08:56:53 -0300

    Optimizations
    Optimizations and Bug Fixes no cadastro de locais.

--- cadastro_local.php
+++ cadastro_local.php
@@ -1,91 +1,100 @@
+<?php
+include "conexao.php"; 
+
+// variaveis
+$page = "cadastro_local.php";
+$btnUpdate = "btnUpdate";
+$btnInsert = "btnInsert";
+$btnDelete = "btnDelete";
+$modalInsert = "insert-local";
+$modalUpdate = "update-local";
+$dataInput = "inputLocal";
+$dataId = "idLocal";
+$duplicate = false;
+$blocked = false;
+$sqlTabLocal = "local";
+$sqlTabSetor = "setor";
+$sqlOrder = "ORDER BY LOWER(nome)";
+
+// busca a lista de locais no banco
+$locais = db_select("SELECT * FROM ".$sqlTabLocal." ".$sqlOrder);
+
+// altera locais no banco
+if(isset($_POST[$btnUpdate])){
+	$result = db_query("UPDATE ".$sqlTabLocal." SET nome = ".db_quote($_POST[$dataInput])." WHERE id = ".db_quote($_POST[$dataId]));
+	if($result === false) {
+		$error = pg_result_error($result);
+	}
+	header("Refresh:0");
+}
+
+// exclui locais do banco
+if(isset($_POST[$btnDelete])){
+	// testa se não existe dependências
+	$blocked = false;
+	$locaisBlocked = db_select("SELECT DISTINCT idLocal FROM ".$sqlTabSetor);
+	for ($i = 0; $i < count($locaisBlocked); $i++){
+		if(strcasecmp($_POST[$dataId], $locaisBlocked[$i]['idlocal']) == 0){
+			$blocked = true;
+		}
+	}
+
+	// se não existe deleta o local
+	if($blocked === false){
+		$result = db_query("DELETE from ".$sqlTabLocal." WHERE id = ".db_quote($_POST[$dataId]));
+		if($result === false) {
+			$error = pg_result_error($result);
+		}
+		header("Refresh:0");	
+	}
+}
+
+// insere locais no banco
+if(isset($_POST[$btnInsert])){
+	// testa se já não existe uma entrada duplicada (case insensitive)
+	$duplicate = false;
+	for ($i = 0; $i < count($locais); $i++) {
+		if(strcasecmp($locais[$i]['nome'], $_POST[$dataInput]) == 0){
+			$duplicate = true;
+		}
+	}
+
+	// se não existe insere no banco
+	if($duplicate == false){
+		$result = db_query("INSERT INTO ".$sqlTabLocal." (nome) VALUES (".db_quote($_POST[$dataInput]).")");
+		if($result === false) {
+			$error = pg_result_error($result);
+		}
+		// atualiza o array com a lista de locais
+		$locais = db_select("SELECT * FROM ".$sqlTabLocal." ".$sqlOrder);
+		header("Refresh:0");	
+	}
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<!-- PHP Includes -->
-	<?php 
-		include "conexao.php"; 
-		//buscando a lista de locais no banco
-		$tabLocal = "local";
-		$tabSetor = "setor";
-		$locais = db_select("SELECT * FROM ".$tabLocal." ORDER BY LOWER(nome)");
-
-		// salvando alteracao de local no banco
-		if(isset($_POST['submit-local'])){
-			$result = db_query("UPDATE ".$tabLocal." SET nome = ".db_quote($_POST['inputLocal'])." WHERE id = ".db_quote($_POST['idLocal']));
-			if($result === false) {
-				$error = pg_result_error($result);
-			}
-			header("Refresh:0");
-		}
-
-		// salvando exclusao de local no banco
-		if(isset($_POST['delete-local'])){
-			// testa se não existe dependências
-			$blocked = false;
-			$locaisBlocked = db_select("SELECT DISTINCT idLocal FROM ".$tabSetor);
-			for ($i = 0; $i < count($locaisBlocked); $i++){
-				if(strcasecmp($_POST['idLocal'], $locaisBlocked[$i]['idlocal']) == 0){
-					$blocked = true;
-					$blockedError = "Yes";
-				}
-			}
-
-			// se não existe deleta o local
-			if($blocked == false){
-				$result = db_query("DELETE from ".$tabLocal." WHERE id = ".db_quote($_POST['idLocal']));
-				if($result === false) {
-					$error = pg_result_error($result);
-				}
-				header("Refresh:0");	
-			}
-		}
-
-		// salvando insercao de local no banco
-		if(isset($_POST['insert-local'])){
-			// testa se já não existe uma entrada duplicada (case insensitive)
-			$duplicate = false;
-			for ($i = 0; $i < count($locais); $i++) {
-				if(strcasecmp($locais[$i]['nome'], $_POST['inputLocal']) == 0){
-					$duplicate = true;
-					$duplicateError = "Yes";
-				}
-			}
-
-			// se não existe insere no banco
-			if($duplicate == false){
-				$result = db_query("INSERT INTO ".$tabLocal." (nome) VALUES (".db_quote($_POST['inputLocal']).")");
-				if($result === false) {
-					$error = pg_result_error($result);
-				}
-				// atualiza o array com a lista de locais
-				$locais = db_select("SELECT * FROM ".$tabLocal." ORDER BY LOWER(nome)");
-				header("Refresh:0");	
-			}
-		}
-	?>
-
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head -->
-	<title>Cadastro de Funções</title>
-
-	<!-- CSS Styles -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cadastro de Funções</title>
+ 
+    <!-- CSS Styles -->
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap-datepicker.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css" />
 	<link rel="stylesheet" type="text/css" href="css/custom.css">
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
+ 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-	<!-- jQuery -->
+    <!-- jQuery & JavaScript -->
 	<!-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
 	<script type="text/javascript" src="js/jquery1-11-3.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
@@ -99,12 +108,11 @@
 			<div class="navbar-header">
 				<a class="navbar-brand" href="index.php">SIGOI</a>
 			</div>
-
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<!--  Barra de Navegação: Esquerda -->
 				<ul class="nav navbar-nav">
 					<li class="nav nav-btn"><a href="index.php">Sair</a></li>
-					<li class="nav nav-btn" data-toggle="modal" data-target="#insertLocal"><a href="#">Incluir Local</a></li>
+					<li class="nav nav-btn" data-toggle="modal" <?php echo(" data-target=\"#".$modalInsert."\""); ?>><a href="#">Incluir Local</a></li>
 				</ul>
 				<!-- Barra de Navegação: Direita -->
 				<ul class="nav navbar-nav navbar-right">
@@ -115,51 +123,50 @@
 
 	<!-- Conteúdo -->
 	<div class="container-fluid">
-	<div class="row">
+		<div class="row">
+			<!-- Mensagem de Erro ao cadastrar local duplicada -->
+			<?php if($duplicate === true){ ?>
+			<div class="col-md-10 col-md-offset-1">
+				<div class="alert alert-danger alert-dismissible" role="alert">
+					<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<strong>Atenção!</strong> Esse local já existe no cadastro.
+				</div>
+			</div>
+			<?php } ?>
 
-	<!-- Mensagem de Erro ao cadastrar local duplicada -->
-	<?php if(!empty($duplicateError)){ ?>
-	<div class="col-md-10 col-md-offset-1">
-		<div class="alert alert-danger alert-dismissible" role="alert">
-			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			<strong>Atenção!</strong> Esse local já existe no cadastro.
-		</div>
-	</div>
-	<?php } ?>
+			<!-- Mensagem de Erro ao tentar deletar um local com dependências -->
+			<?php if($blocked === true){ ?>
+			<div class="col-md-10 col-md-offset-1">
+				<div class="alert alert-danger alert-dismissible" role="alert">
+					<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<strong>Atenção!</strong> Esse local está vinculado a um ou mais setores. Não é possível efetuar a exclusão.
+				</div>
+			</div>
+			<?php } ?>
+				
+			<!-- Tabela com a lista de locais -->
+			<table class="table table-condensed table-hover">
+				<thead>
+					<tr>
+						<th width="1%"></th>
+						<th class="col-sm-3">Local</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+					for ($i = 0; $i < count($locais); $i++) {
+						echo "<tr data-toggle=\"modal\" data-id=\"".$locais[$i]['id']."\" data-target=\"#".$modalUpdate."\" data-raw=\"".$locais[$i]['nome']."\">";
+						echo "<td></td>";
+						echo "<td>".$locais[$i]['nome']."</td>";
+						echo "</tr>";
+					} ?>
+				</tbody>
+			</table>
+		</div> <!-- /Row -->
+	</div> <!-- /Container-Fluid -->
 
-	<!-- Mensagem de Erro ao tentar deletar um local com dependências -->
-	<?php if(!empty($blockedError)){ ?>
-	<div class="col-md-10 col-md-offset-1">
-		<div class="alert alert-danger alert-dismissible" role="alert">
-			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			<strong>Atenção!</strong> Esse local está vinculado a um ou mais setores. Não é possível efetuar a exclusão.
-		</div>
-	</div>
-	<?php } ?>
-		
-		<table class="table table-condensed table-hover">
-			<thead>
-				<tr>
-					<th width="1%"></th>
-					<th class="col-sm-3">Local</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php 
-				for ($i = 0; $i < count($locais); $i++) {
-					echo "<tr data-toggle=\"modal\" data-id=\"".$locais[$i]['id']."\" data-target=\"#editLocal\" data-raw=\"".$locais[$i]['nome']."\">";
-					echo "<td></td>";
-					echo "<td>".$locais[$i]['nome']."</td>";
-					echo "</tr>";
-				} ?>
-			</tbody>
-		</table>
-
-	</div> <!-- Entire Row -->
-	</div> <!-- Container-Fluid -->
-
-	<!-- Modal Edit Local -->
-	<div class="modal fade" id="editLocal" tabindex="-1" role="dialog">
+	<!-- Modal update-local -->
+	<div class="modal fade" <?php echo(" id=\"".$modalUpdate."\""); ?> tabindex="-1" role="dialog">
 		<div class="modal-dialog" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
@@ -167,16 +174,16 @@
 					<h4 class="modal-title">Editar Local</h4>
 				</div>
 				<div class="modal-body">
-					<form role="form" method="post" action="cadastro_local.php">
+					<form role="form" method="post" <?php echo(" action=\"".$page."\""); ?>>
 						<div class="form-group">
 							<label for="local-heading">Função</label>
-							<input type="hidden" name="idLocal" id="idLocal" value=""/>
-							<input type="text" name="inputLocal" class="form-control" value="" id="inputLocal">
+							<input type="hidden" <?php echo(" name=\"".$dataId."\""); ?> <?php echo(" id=\"".$dataId."\""); ?> value=""/>
+							<input type="text" name=<?php echo("\"".$dataInput."\""); ?> class="form-control" value="" <?php echo(" id=\"".$dataInput."\""); ?>>
 						</div>
 						<div class="form-group">
 							<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                			<input name="submit-local" type="submit" class="btn btn-primary" value="Salvar"/>
-                			<input name="delete-local" type="submit" class="btn btn-danger" value="Delete" onclick="return confirm('Você tem certeza?');"/>
+                			<input <?php echo(" name=\"".$btnUpdate."\""); ?> type="submit" class="btn btn-primary" value="Salvar"/>
+                			<input <?php echo(" name=\"".$btnDelete."\""); ?> type="submit" class="btn btn-danger" value="Delete" onclick="return confirm('Você tem certeza?');"/>
         				</div>
 					</form>
 				</div>
@@ -184,8 +191,8 @@
 		</div>
 	</div>
 
-	<!-- Modal Insert local -->
-	<div class="modal fade" id="insertLocal" tabindex="-1" role="dialog">
+	<!-- Modal insert-local -->
+	<div class="modal fade" <?php echo(" id=\"".$modalInsert."\""); ?> tabindex="-1" role="dialog">
 		<div class="modal-dialog" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
@@ -193,14 +200,14 @@
 					<h4 class="modal-title">Incluir Local</h4>
 				</div>
 				<div class="modal-body">
-					<form role="form" method="post" action="cadastro_local.php">
+					<form role="form" method="post" <?php echo(" action=\"".$page."\""); ?>>
 						<div class="form-group">
 							<label for="local-heading">Local</label>
-							<input type="text" name="inputLocal" class="form-control" value="" id="inputLocal">
+							<input type="text" <?php echo(" name=\"".$dataInput."\""); ?> class="form-control" value="" <?php echo(" id=\"".$dataInput."\""); ?>>
 						</div>
 						<div class="form-group">
 							<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                			<input name="insert-local" type="submit" class="btn btn-primary" value="Salvar"/>
+                			<input <?php echo(" name=\"".$btnInsert."\""); ?> type="submit" class="btn btn-primary" value="Salvar"/>
         				</div>
 					</form>
 				</div>
@@ -208,24 +215,25 @@
 		</div>
 	</div>
 
-	<script>
-    $('tr').on('click', function (e) {
-	    e.preventDefault();
-	    // pegando o id e o nome da local na linha clicada
-	    var id = $(this).closest('tr').data('id');
-	    var nome = $(this).closest('tr').data('raw');
-	    // mandando isso pra dentro do modal
-	    $("#editLocal #idLocal").val(id);
-	    $("#editLocal #inputLocal").val(nome);
-	});
+	<!-- Scripts -->
+	<script type="text/javascript">
+	    $('tr').on('click', function (e) {
+		    e.preventDefault();
+		    // pegando o id e o nome da local na linha clicada
+		    var id = $(this).closest('tr').data('id');
+		    var nome = $(this).closest('tr').data('raw');
+		    // mandando isso pra dentro do modal
+		    $("#update-local #idLocal").val(id);
+		    $("#update-local #inputLocal").val(nome);
+		});
 
-    // seta o foco pro text field inputLocal
-	$("#insertLocal").on('shown.bs.modal', function(){
-        $(this).find('#inputLocal').focus();
-    });
-    $("#editLocal").on('shown.bs.modal', function(){
-        $(this).find('#inputLocal').focus();
-    });
+	    // seta o foco pro text field inputLocal
+		$("#insert-local").on('shown.bs.modal', function(){
+	        $(this).find('#inputLocal').focus();
+	    });
+	    $("#update-local").on('shown.bs.modal', function(){
+	        $(this).find('#inputLocal').focus();
+	    });
 	</script>
 
 </body>
